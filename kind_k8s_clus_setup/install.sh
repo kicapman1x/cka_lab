d5c4b84874635c708bd6dee3e8b926fe93714820
kicapman1x <ahhannan995@gmail.com>
2025-08-31 18:02:57 +0800

    adding alias for convenience

--- kind_k8s_clus_setup/install.sh
+++ kind_k8s_clus_setup/install.sh
@@ -2,6 +2,13 @@
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64 && chmod +x ./kind && sudo mv ./kind/usr/local/bin/kind
 
-sudo kind create cluster --image kindest/node:v1.33.2 --config ./cluster.yaml --name cka-cluster
+#name u can put whatever
+sudo kind create cluster --image kindest/node:v1.33.2 --config ./cluster.yaml --name k8s-cluster
 
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && sudo chmod +x kubectl && sudo mv kubectl /usr/local/bin/kubectl
+
+#the nomenclature for container will be <cluster name>-<control-plane/worker#>
+#you may insert the desired restart policy lol I just dont want it to restart always
+sudo docker update --restart no k8s-cluster-worker k8s-cluster-worker2 k8s-cluster-control-plane
+
+source kindvars.env
