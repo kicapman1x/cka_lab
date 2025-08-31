62643db5c3987551e70ee993947dbeb57144c25e
kicapman1x <ahhannan995@gmail.com>
2025-08-31 15:00:39 +0800

    kind create cluster needs sudo, downloading kubectl as well

--- kind_k8s_clus_setup/install.sh
+++ kind_k8s_clus_setup/install.sh
@@ -2,4 +2,6 @@
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64 && chmod +x ./kind && sudo mv ./kind/usr/local/bin/kind
 
-kind create cluster --image kindest/node:v1.33.2 --config ./cluster.yaml --name cka-cluster
+sudo kind create cluster --image kindest/node:v1.33.2 --config ./cluster.yaml --name cka-cluster
+
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && sudo chmod +x kubectl && sudo mv kubectl /usr/local/bin/kubectl
